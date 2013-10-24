2c4e5de0e656a013d4f136ccb340fe373f20a18d
Chris Field <christopherdfield@gmail.com>
2013-10-24 16:59:59 -0400

    Genesis Template Page
    Continuing on doing this in Genesis, create a new php file in your theme's root folder. Name it after the "theme_location" value you use below. This value needs to also match the value used in the section above.

---
+++ genesis-template-page.php
@@ -0,0 +1,12 @@
+// My Custom Template Page
+
+     <?php echo '<div class="widget-wrap">';  
+     wp_nav_menu( array( 
+     'sort_column' => 'menu_order', 
+     'container_id' => 'interior-nav' , 
+     'menu_class' => 'menu', 
+     'theme_location' => 'subnavone') ); 
+      echo '</div>'; 
+      ?>
+
+     // http://journalxtra.com/easyguides/add-menu-genesis-themes/
